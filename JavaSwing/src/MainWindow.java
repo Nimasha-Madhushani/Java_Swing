dc2c798accd17548b3da3090b1452876e6b2a7ba
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-02-10 23:40:01 +0530

    create a window

---
+++ JavaSwing/src/MainWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainWindow {
+    private JFrame window;
+    public MainWindow(){
+        window=new JFrame();
+        window.setTitle("Hello");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(800,500);
+        window.setLocationRelativeTo(null);
+
+
+    }
+    public void show(){
+        window.setVisible(true);
+
+    }
+}
