5d96b1c552a4e8c78409cf7a7787605e5c95a89f
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-02-13 09:32:57 +0530

    create flowlayout in a loop

--- JavaSwing/src/Flow_Layout.java
+++ JavaSwing/src/Flow_Layout.java
@@ -14,7 +14,12 @@ public class Flow_Layout {
         jFrame.setTitle("Flow Layout");
         jPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
         BorderLayout borderLayout = new BorderLayout();
-jPanel.setBackground(Color.pink);
+        jPanel.setBackground(Color.pink);
+        for (int i = 1; i <= 5; i++) {
+            JButton jButton = new JButton("Button" + Integer.toString(i));
+            jPanel.add(jButton);
+        }
+
         jFrame.setLayout(new BorderLayout());
         jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         jFrame.setSize(800, 500);
